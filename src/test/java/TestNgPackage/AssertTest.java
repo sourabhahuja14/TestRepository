ace157e704335cf9748d78b8ef92264e65b74adf
Jyotsnsa Arora <Jyotsnsa Arora@LAPTOP-GSN6DV37>
2017-06-17 17:38:13 +0530

    MovingTestingProjecttoGitHub

---
+++ src/test/java/TestNgPackage/AssertTest.java
@@ -0,0 +1,33 @@
+package TestNgPackage;
+
+import org.testng.annotations.Test;
+
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+
+
+
+public class AssertTest {
+	
+	@Test
+	public void hardass()
+	{
+		System.out.println("Hard Assert Started");
+		Assert.assertEquals(67,67,"Values did not Match");
+		System.out.println("Hard Assert Ended");
+		
+	}
+	
+	@Test
+	public void softAssert()
+	{
+		SoftAssert assertion=new SoftAssert();
+		
+		Assert.assertEquals(12,12,"Issue in SoftAssert");
+		
+		System.out.println("Soft Assert Ended");
+		
+		assertion.assertAll();
+	}
+
+}
